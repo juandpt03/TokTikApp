a6d5bca133e06ed532a1c100e6cf4ec33a46c5f5
juandpt03 <juandpt03@gmail.com>
2023-05-01 13:53:00 -0500

    Corrección de assets videos

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 import 'package:toktik/presentation/widgets/video/full_screen_player.dart';
-import 'package:toktik/presentation/widgets/video/video_background.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
