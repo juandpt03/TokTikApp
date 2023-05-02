54471c232816247db988476d4a8b491d6e2a894f
juandpt03 <juandpt03@gmail.com>
2023-05-02 02:45:04 -0500

    imp mejoras

---
+++ lib/infrastructure/repositories/video_post_respository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+
+class VideoPostRespositoryImpl implements VideoPostRepository {
+  final VideoPostDataSource videoDataSource;
+
+  VideoPostRespositoryImpl({required this.videoDataSource});
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    // TODO: implement getFavoriteVideosByUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videoDataSource.getTrendingVideosByPage(page);
+  }
+}
--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
-import 'package:toktik/infrastructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  final VideoPostRepository videoPostRepository;
   //TODO: Repository, DataSource
   bool initialLoading = true;
   List<VideoPost> videos = [];
+
+  DiscoverProvider({required this.videoPostRepository});
   Future<void> loadNextPage() async {
     // await Future.delayed(const Duration(seconds: 2));
 
-    final List<VideoPost> newVideos = videoPosts
-        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
-        .toList();
+    // final List<VideoPost> newVideos = videoPosts
+    //     .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+    //     .toList();
+    List<VideoPost> newVideos =
+        await videoPostRepository.getTrendingVideosByPage(1);
     videos.addAll(newVideos);
     initialLoading = false;
     notifyListeners();
